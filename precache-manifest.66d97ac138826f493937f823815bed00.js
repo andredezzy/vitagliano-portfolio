87baad57c0f87a4166e94719ef0cb9cffe0911dc
André Victor <andredezzy@outlook.com>
2019-02-10 01:05:59 -0200

    Updates

---
+++ precache-manifest.66d97ac138826f493937f823815bed00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2457a79761545a7b94b",
+    "url": "/vitagliano-portfolio/static/css/main.526c3db4.chunk.css"
+  },
+  {
+    "revision": "c2457a79761545a7b94b",
+    "url": "/vitagliano-portfolio/static/js/main.c2457a79.chunk.js"
+  },
+  {
+    "revision": "94e4fe497233a60faa9d",
+    "url": "/vitagliano-portfolio/static/css/1.ca02f7c8.chunk.css"
+  },
+  {
+    "revision": "94e4fe497233a60faa9d",
+    "url": "/vitagliano-portfolio/static/js/1.94e4fe49.chunk.js"
+  },
+  {
+    "revision": "27198ff4c8d60e8682fc",
+    "url": "/vitagliano-portfolio/static/js/runtime~main.27198ff4.js"
+  },
+  {
+    "revision": "4a239189ef89ce8fbf43c54a6e1eefbd",
+    "url": "/vitagliano-portfolio/static/media/logo.4a239189.png"
+  },
+  {
+    "revision": "3cf5ec44f0e50ae19c437a3975dd8a7e",
+    "url": "/vitagliano-portfolio/index.html"
+  }
+];
